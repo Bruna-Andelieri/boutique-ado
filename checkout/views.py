5ec3e015e2ac7378ce6fe4cfc46825060e6dcaf7
Bruna-Andelieri <bruna.andelieri@gmail.com>
2024-03-04 11:08:55 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OolhXIAQ23pze0ryqCYG2dEv5GbeDRRvgTrAfitYhWuciy2yKkPUOxqNSInSmElq1GuXdxrIIEIQtWRQISvSYoy00RqvZ8Uzd',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
